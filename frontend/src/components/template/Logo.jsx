7f5b014727e5d31883ae186383312d81b41fb405
Vitor Ribeiro <vitorribeirovrm@outlook.com>
2021-06-25 00:35:01 -0300

    Primeiro envio
    todos os componentes já foram criados mas as rotas precisam ser criadas assim como o CRUD

---
+++ frontend/src/components/template/Logo.jsx
@@ -0,0 +1,8 @@
+import './Logo.css'
+import logo from '../../assets/img/logo-agronunes2.png'
+import React from 'react'
+
+export default props => 
+    <aside className="logo">
+        <img src={logo} alt="logo" />
+    </aside>
---
+++ frontend/src/components/template/Nav.jsx
@@ -0,0 +1,14 @@
+import './Nav.css'
+import React from 'react'
+
+export default props =>
+    <aside className="menu-area">
+        <nav className="menu">
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Clientes
+            </a>
+        </nav>
+    </aside>
